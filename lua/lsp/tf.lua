77b82fd7d8c2c8fbe8d554ac2d4797ebe772dfb7
Yago Riveiro <yago.riveiro@farfetch.com>
2021-04-07 22:33:16 +0100

    [FEAT] add configurations to terraform lsp

--- lua/lsp/tf.lua
+++ lua/lsp/tf.lua
@@ -3,6 +3,11 @@ local lspconfig = require 'lspconfig'
 lspconfig.terraformls.setup{
 	cmd = {'terraform-ls', 'serve'},
 	settings = {
+		terraformls = {
+			experimentalFeatures = {
+				validateOnSave = true
+			}
+		},
 		terraform = {
 			languageServer = {
 				pathToBinary = '/usr/local/bin/terraform-ls'
