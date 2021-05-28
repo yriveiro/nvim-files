ce29b399d49ccc37861fa17935c0bfd6b065689f
Yago Riveiro <yago.riveiro@gmail.com>
2021-05-28 11:09:26 +0100

    [FEAT] remove hardcoded path and use HOME

--- lua/lsp/bash.lua
+++ lua/lsp/bash.lua
@@ -1,5 +1,5 @@
 local lspconfig = require 'lspconfig'
 
 lspconfig.bashls.setup {
-	cmd= {'/Users/yago.riveiro/.local/share/nvim/lspinstall/bash/node_modules/.bin/bash-language-server', 'start'}
+	cmd= {os.getenv("HOME") .. '.local/share/nvim/lspinstall/bash/node_modules/.bin/bash-language-server', 'start'}
 }
--- lua/lsp/yaml.lua
+++ lua/lsp/yaml.lua
@@ -1,6 +1,6 @@
 local lspconfig = require 'lspconfig'
 
 lspconfig.yamlls.setup{
-  cmd= {'/Users/yago.riveiro/.local/share/nvim/lspinstall/yaml/node_modules/.bin/yaml-language-server', '--stdio'},
+  cmd= {os.getenv("HOME") .. '.local/share/nvim/lspinstall/yaml/node_modules/.bin/yaml-language-server', '--stdio'},
   filetypes = { 'yaml', 'helm', 'yml' }
 }
