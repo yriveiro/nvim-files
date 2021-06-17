7e6e2f764b16e98d6961c8e2cf2a5a81beceaa4b
Yago Riveiro <yago.riveiro@gmail.com>
2021-06-17 08:46:31 +0000

    [FIX] better LSP mapping

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -11,12 +11,12 @@ local api = vim.api
 local opts = {noremap = true, silent = true}
 
 -- LSP Mappings
-api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-api.nvim_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-api.nvim_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-api.nvim_set_keymap('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-api.nvim_set_keymap('n', '1gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-api.nvim_set_keymap('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
-api.nvim_set_keymap('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
-api.nvim_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+api.nvim_set_keymap('n', ',d', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+api.nvim_set_keymap('n', ',h', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+api.nvim_set_keymap('n', ',i', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+api.nvim_set_keymap('n', ',s', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+api.nvim_set_keymap('n', ',td', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+api.nvim_set_keymap('n', ',r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+api.nvim_set_keymap('n', ',ds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+api.nvim_set_keymap('n', ',w', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+api.nvim_set_keymap('n', ',f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
