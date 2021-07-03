47ef21929866cf0edbaea127e33c259fc0ee2131
Yago Riveiro <yago.riveiro@gmail.com>
2021-07-03 13:53:23 +0000

    [FIX] keymap for nvimtree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -3,51 +3,57 @@ local g = vim.g
 local opts = {noremap = true, silent = true}
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
-g.nvim_tree_width = 40
-g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
-g.nvim_tree_gitignore = 1
+g.nvim_tree_add_trailing = 1
 g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
 g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ':~'
-g.nvim_tree_tab_open = 1
-g.nvim_tree_add_trailing = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_gitignore = 1
 g.nvim_tree_group_empty = 1
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_indent_markers = 1
 g.nvim_tree_lsp_diagnostics = 1
+g.nvim_tree_quit_on_open = 1
+g.nvim_tree_root_folder_modifier = ':~'
 g.nvim_tree_special_files = { 'README.md', 'Makefile', 'MAKEFILE' }
+g.nvim_tree_tab_open = 1
+g.nvim_tree_width = 50
 
 vim.g.nvim_tree_bindings = {
-  ["<CR>"]           = tree_cb("edit"),
-  ["o"]              = tree_cb("edit"),
-  ["<2-LeftMouse>"]  = tree_cb("edit"),
-  ["<2-RightMouse>"] = tree_cb("cd"),
-  ["<C-]>"]          = tree_cb("cd"),
-  ["v"]              = tree_cb("vsplit"),
-  ["h"]              = tree_cb("split"),
-  ["t"]              = tree_cb("tabnew"),
-  ["<"]              = tree_cb("prev_sibling"),
-  [">"]              = tree_cb("next_sibling"),
-  ["<BS>"]           = tree_cb("close_node"),
-  ["<S-CR>"]         = tree_cb("close_node"),
-  ["<Tab>"]          = tree_cb("preview"),
-  ["I"]              = tree_cb("toggle_ignored"),
-  ["H"]              = tree_cb("toggle_dotfiles"),
-  ["R"]              = tree_cb("refresh"),
-  ["a"]              = tree_cb("create"),
-  ["d"]              = tree_cb("remove"),
-  ["r"]              = tree_cb("rename"),
-  ["<C-r>"]          = tree_cb("full_rename"),
-  ["x"]              = tree_cb("cut"),
-  ["c"]              = tree_cb("copy"),
-  ["p"]              = tree_cb("Paste"),
-  ["[c"]             = tree_cb("prev_git_item"),
-  ["]c"]             = tree_cb("next_git_item"),
-  ["-"]              = tree_cb("dir_up"),
-  ["q"]              = tree_cb("close"),
+  { key = {"<CR>"}, cb = tree_cb("edit") },
+  { key = {"+"},    cb = tree_cb("cd") },
+  { key = "v",      cb = tree_cb("vsplit") },
+  { key = "h",      cb = tree_cb("split") },
+  { key = "t",      cb = tree_cb("tabnew") },
+  { key = "<",      cb = tree_cb("prev_sibling") },
+  { key = ">",      cb = tree_cb("next_sibling") },
+  { key = "P",      cb = tree_cb("parent_node") },
+  { key = "<BS>",   cb = tree_cb("close_node") },
+  { key = "<S-CR>", cb = tree_cb("close_node") },
+  { key = "<Tab>",  cb = tree_cb("preview") },
+  { key = "K",      cb = tree_cb("first_sibling") },
+  { key = "J",      cb = tree_cb("last_sibling") },
+  { key = "I",      cb = tree_cb("toggle_ignored") },
+  { key = "H",      cb = tree_cb("toggle_dotfiles") },
+  { key = "R",      cb = tree_cb("refresh") },
+  { key = "a",      cb = tree_cb("create") },
+  { key = "d",      cb = tree_cb("remove") },
+  { key = "r",      cb = tree_cb("rename") },
+  { key = "<C-r>",  cb = tree_cb("full_rename") },
+  { key = "x",      cb = tree_cb("cut") },
+  { key = "c",      cb = tree_cb("copy") },
+  { key = "p",      cb = tree_cb("paste") },
+  { key = "y",      cb = tree_cb("copy_name") },
+  { key = "Y",      cb = tree_cb("copy_path") },
+  { key = "gy",     cb = tree_cb("copy_absolute_path") },
+  { key = "<c",     cb = tree_cb("prev_git_item") },
+  { key = ">c",     cb = tree_cb("next_git_item") },
+  { key = "-",      cb = tree_cb("dir_up") },
+  { key = "q",      cb = tree_cb("close") },
+  { key = "?",      cb = tree_cb("toggle_help") },
 }
-
 
 api.nvim_set_keymap("n", "§", ":NvimTreeToggle<CR>", opts)
 api.nvim_set_keymap("n", "<leader>§", ":NvimTreeFindFile<CR>", opts)
