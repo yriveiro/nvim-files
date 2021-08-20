226057744c230ba6f3ce5de7c13ea89c479d3529
Yago Riveiro <yago.riveiro@gmail.com>
2021-08-20 17:33:32 +0000

    [FIX] missing slash

--- lua/lsp/bash.lua
+++ lua/lsp/bash.lua
@@ -1,5 +1,5 @@
 local lspconfig = require 'lspconfig'
 
 lspconfig.bashls.setup {
-	cmd= {os.getenv("HOME") .. '.local/share/nvim/lspinstall/bash/node_modules/.bin/bash-language-server', 'start'}
+	cmd= {os.getenv("HOME") .. '/.local/share/nvim/lspinstall/bash/node_modules/.bin/bash-language-server', 'start'}
 }
