961e3828f23c2f2fe450b45913fe8dc87ae3ace9
Yago Riveiro <yago.riveiro@gmail.com>
2021-08-20 08:09:53 +0000

    [FEAT] improve leader configuration

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -2,20 +2,27 @@ require 'settings/keybinds'
 require 'settings/autocmds'
 
 local api = vim.api
+local o = vim.o
+local g = vim.g
 
 api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-vim.o.mouse = "a"             -- enable mouse
-vim.o.showcmd = true          -- Show (partial) command in status line.
-vim.o.number = true           -- Line numbers
-vim.o.relativenumber = true   -- Line relative numbers
-vim.o.termguicolors = true    -- Enable true colors
-vim.o.synmaxcol = 2048        -- Set a limit for syntax highlights
-vim.o.laststatus = 2          -- Display the status line always
-vim.o.linespace = 0           -- Set line-spacing to minimum.
-vim.o.showmode = true         -- Show current mode.
-vim.o.cursorline  = true      -- Highlight the screen line of the cursor.
-vim.o.splitbelow = true
-vim.o.splitright = true       -- Split configurations
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+-- Leader key configuation
+-- https://learnvimscriptthehardway.stevelosh.com/chapters/06.html
+g.mapleader = "\\"        -- define leader key
+g.localmapleader = ","    -- define local leader
+
+o.mouse = "a"             -- enable mouse
+o.showcmd = true          -- Show (partial) command in status line.
+o.number = true           -- Line numbers
+o.relativenumber = true   -- Line relative numbers
+o.termguicolors = true    -- Enable true colors
+o.synmaxcol = 2048        -- Set a limit for syntax highlights
+o.laststatus = 2          -- Display the status line always
+o.linespace = 0           -- Set line-spacing to minimum.
+o.showmode = true         -- Show current mode.
+o.cursorline  = true      -- Highlight the screen line of the cursor.
+o.splitbelow = true
+o.splitright = true       -- Split configurations
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
