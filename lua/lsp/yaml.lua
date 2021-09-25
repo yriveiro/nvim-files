9578ab3379260fe62ba6c17d5bbf047b04b0c915
Yago Riveiro <yago.riveiro@gmail.com>
2021-09-25 14:03:55 +0000

    [FEAT] configure schemas for argo and k8s

--- lua/lsp/yaml.lua
+++ lua/lsp/yaml.lua
@@ -1,6 +1,19 @@
 local lspconfig = require 'lspconfig'
+local server = '/.local/share/nvim/lspinstall/yaml/node_modules/.bin/yaml-language-server'
+
 
 lspconfig.yamlls.setup{
-  cmd= {os.getenv("HOME") .. '/.local/share/nvim/lspinstall/yaml/node_modules/.bin/yaml-language-server', '--stdio'},
-  filetypes = { 'yaml', 'helm', 'yml' }
+  filetypes = { 'yaml', 'helm', 'yml' },
+  settings = {
+    yaml = {
+      schemas = {
+        kubernetes = {"*.yml", "*.yaml"},
+        ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = "*.argo.yaml",
+      },
+    },
+    cmd = {
+      os.getenv("HOME") .. server,
+      '--stdio'
+    },
+  },
 }
