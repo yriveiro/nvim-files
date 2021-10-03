47628b849cae8af33ee8dd4ee9d2b63b5365981c
Yago Riveiro <yago.riveiro@gmail.com>
2021-10-03 09:21:41 +0000

    [FIX] regresion on lua LSP

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -5,6 +5,9 @@ if vim.fn.has("mac") == 1 then
   system_name = "macOS"
 elseif vim.fn.has("unix") == 1 then
   system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
   print("Unsupported system for sumneko")
 end
 
