d9daafe8789654e33239cedc41c5302aa8fd7911
Yago Riveiro <yago.riveiro@gmail.com>
2021-10-31 11:48:22 +0000

    [FEAT] move keybinding definition to wichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -7,11 +7,21 @@ wk.register({
     r = { "<cmd>lua require('telescope.builtin').live_grep()<CR>", "Live Grep"},
     g = { "<cmd>lua require('telescope.builtin').git_commits()<CR>", "Find Git Commit"},
   },
-  t = { name = "FloatTerm Toogle" },
+  t = { name = "+Floatterm" },
   b = { "<cmd>Vista!!<CR>", "Vista" },
   ["§"] = { ":NvimTreeFindFile<CR>", "Find file in NvimTree" }
 }, { prefix = "<leader>" })
 
 wk.register({
-  ["§"] = { ":NvimTreeToggle<CR>", "Toggle NvimTree" }
+  t = {
+    y = { ":tabnext<CR>", "Next tab"},
+    r = { ":tabnext<CR>", "Previous tab"},
+    n = { ":tabnew<CR>", "New Tab"}
+  },
+  ["§"] = { ":NvimTreeToggle<CR>", "Toggle NvimTree" },
+  ch = { ":CloseHiddenBuffers<CR>", "Close Hidden Buffers" }
 })
+
+wk.register({
+  e = { "<cmd>lua require('vim.diagnostic').show_line_diagnostics()<CR>", "Show Diagnostics" },
+}, { prefix = "<localeader>" })
--- lua/settings/keybinds.lua
+++
@@ -1,8 +0,0 @@
-local api = vim.api
-local opts = {noremap = true, silent = true}
-
-api.nvim_set_keymap("n", "ty", ":tabnext<CR>", opts)
-api.nvim_set_keymap("n", "tr", ":tabprev<CR>", opts)
-api.nvim_set_keymap("n", "tn", ":tabnew<CR>", opts)
-api.nvim_set_keymap("n", "ch", ":CloseHiddenBuffers<CR>", opts)
-api.nvim_set_keymap("n", "<localleader>e", "<cmd>lua require('vim.diagnostic').show_line_diagnostics()<CR>", opts)
