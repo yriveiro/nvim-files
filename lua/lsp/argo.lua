3f782a5fa58e2b02e713ee58d6a590b8b9f8f80d
Yago Riveiro <yago.riveiro@gmail.com>
2022-01-01 01:54:44 +0000

    feat: rework lsp configurations

---
+++ lua/lsp/argo.lua
@@ -0,0 +1,27 @@
+local servers = require'nvim-lsp-installer.servers'
+
+local ok, server = servers.get_server('argo')
+if ok then
+  server:on_ready(function ()
+    local opts = {
+      settings = {
+        -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
+        redhat = {
+          telemetry = {
+            enabled = false
+          }
+        },
+        yaml = {
+          schemas = {
+            ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*.argo.{yml,yaml}',
+          },
+        },
+      }
+    }
+    server:setup(opts)
+  end)
+  if not server:is_installed() then
+      -- Queue the server to be installed
+      server:install()
+  end
+end
