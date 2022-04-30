820af82eab5d6010683a5a4aaac23e2936633793
Yago Riveiro <yago.riveiro@gmail.com>
2022-04-30 17:05:33 +0100

    feat(lsp-installer): add default lsp servers

--- lua/plugins/lspinstaller.lua
+++ lua/plugins/lspinstaller.lua
@@ -1,1 +1,20 @@
-require("nvim-lsp-installer").setup {}
+require'nvim-lsp-installer'.setup({
+  ensure_installed = {
+    'bashls',
+    'dockerls',
+    'jsonls',
+    'rust_analyzer',
+    'sumneko_lua',
+    'yamlls',
+    'gopls',
+    'grammarly',
+    'jdtls',
+    'jsonls',
+    'sqls',
+    'sumneko_lua',
+    'taplo',
+    'terraformls',
+    'tflint',
+    'yamlls',
+  },
+})
