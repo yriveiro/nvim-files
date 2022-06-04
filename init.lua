1b38f9c3053dc84c005a7d7ffad86b9947e0257c
Yago Riveiro <yago.riveiro@gmail.com>
2022-06-04 16:18:52 +0100

    feat(plugins): add impatient plugin

--- init.lua
+++ init.lua
@@ -5,9 +5,10 @@
 -- 2. Extend functionality where needed
 -- 3. Change functionality only when it doesn't make sense
 
-require "settings"
-require "plugins"
-require "lsp"
-require "mappings"
-require "autocmds"
-require "commands"
+require 'impatient'.
+require 'settings'
+require 'plugins'
+require 'lsp'
+require 'mappings'
+require 'autocmds'
+require 'commands'
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,13 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = vim.fn.system {
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path,
-    }
+  PACKER_BOOTSTRAP = vim.fn.system {
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path,
+  }
 end
 
 vim.cmd [[packadd packer.nvim]]
@@ -15,146 +15,148 @@ vim.cmd [[packadd packer.nvim]]
 local ok, packer = pcall(require, 'packer')
 
 if not ok then
-    return
+  return
 end
 
 packer.init {
-    display = {
-        open_fn = function()
-            return require('packer.util').float { border = 'single' }
-        end,
-        prompt_border = 'single',
-    },
-    git = {
-        clone_timeout = 600,
-    },
-    auto_clean = true,
-    compile_on_sync = false,
+  display = {
+    open_fn = function()
+      return require('packer.util').float { border = 'single' }
+    end,
+    prompt_border = 'single',
+  },
+  git = {
+    clone_timeout = 600,
+  },
+  auto_clean = true,
+  compile_on_sync = false,
 }
 
 return packer.startup(function(use)
-    use { 'wbthomason/packer.nvim' }
+  -- System
+  use { 'wbthomason/packer.nvim' }
+  use { 'lewis6991/impatient.nvim' }
 
-    -- UI
-    use { 'rmehri01/onenord.nvim' }
-    use { 'MunifTanjim/nui.nvim' }
-    use { 'yriveiro/termim.nvim' }
-    use { 'rcarriga/nvim-notify', config = require 'plugins.configs.notify' }
-    use { 'kyazdani42/nvim-web-devicons', config = require 'plugins.configs.icons' }
-    use {
-      'goolord/alpha-nvim',
-      requires = { 'kyazdani42/nvim-web-devicons' },
-      config = require 'plugins.configs.alpha',
-    }
-    use { 'NTBBloodbath/galaxyline.nvim', config = require 'plugins.configs.galaxyline'}
-    use {
-      'sindrets/diffview.nvim',
-      requires = {
-        'nvim-lua/plenary.nvim',
-        'kyazdani42/nvim-web-devicons',
-      },
-      config = require 'plugins.configs.diffview'
-    }
+  -- UI
+  use { 'rmehri01/onenord.nvim' }
+  use { 'MunifTanjim/nui.nvim' }
+  use { 'yriveiro/termim.nvim' }
+  use { 'rcarriga/nvim-notify', config = require 'plugins.configs.notify' }
+  use { 'kyazdani42/nvim-web-devicons', config = require 'plugins.configs.icons' }
+  use {
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = require 'plugins.configs.alpha',
+  }
+  use { 'NTBBloodbath/galaxyline.nvim', config = require 'plugins.configs.galaxyline' }
+  use {
+    'sindrets/diffview.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = require 'plugins.configs.diffview'
+  }
 
-    -- Search and Navigation
-    use {
+  -- Search and Navigation
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = require 'plugins.configs.telescope',
+  }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'folke/which-key.nvim' }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = require 'plugins.configs.nvimtree',
+  }
+  use { 'mg979/vim-visual-multi' }
+  use { 'liuchengxu/vista.vim' }
+
+  -- Utilities
+  use { 'windwp/nvim-autopairs', config = require 'plugins.configs.autopairs' }
+  use { 'editorconfig/editorconfig-vim' }
+  use { 'McAuleyPenney/tidy.nvim' }
+  use { 'amirali/yapf.nvim', config = require 'plugins.configs.autopairs' }
+  use { 'mbbill/undotree' }
+  use { 'wakatime/vim-wakatime' }
+  use { 'terrortylor/nvim-comment', config = require 'plugins.configs.nvimcomment' }
+
+  -- Debug
+  use {
+    'mfussenegger/nvim-dap',
+    requires = {
+      'rcarriga/nvim-dap-ui',
+      'yriveiro/dap-go.nvim',
+      'theHamsta/nvim-dap-virtual-text',
+    },
+    config = require 'plugins.configs.dap'
+  }
+  use {
+    'nvim-telescope/telescope-dap.nvim',
+    requires = {
       'nvim-telescope/telescope.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = require 'plugins.configs.telescope',
-    }
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use { 'folke/which-key.nvim' }
-    use {
-      'kyazdani42/nvim-tree.lua',
-      requires = { 'kyazdani42/nvim-web-devicons' },
-      config = require 'plugins.configs.nvimtree',
-    }
-    use { 'mg979/vim-visual-multi' }
-    use { 'liuchengxu/vista.vim' }
+      'mfussenegger/nvim-dap'
+    },
+  }
 
-    -- Utilities
-    use { 'windwp/nvim-autopairs', config = require 'plugins.configs.autopairs' }
-    use { 'editorconfig/editorconfig-vim' }
-    use { 'McAuleyPenney/tidy.nvim' }
-    use { 'amirali/yapf.nvim', config = require 'plugins.configs.autopairs' }
-    use { 'mbbill/undotree' }
-    use { 'wakatime/vim-wakatime' }
-    use { 'terrortylor/nvim-comment', config = require 'plugins.configs.nvimcomment' }
+  -- Syntax
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = require 'plugins.configs.treesitter',
+  }
 
-    -- Debug
-    use {
-      'mfussenegger/nvim-dap',
-      requires = {
-        'rcarriga/nvim-dap-ui',
-        'yriveiro/dap-go.nvim',
-        'theHamsta/nvim-dap-virtual-text',
-      },
-      config = require 'plugins.configs.dap'
-    }
-    use {
-      'nvim-telescope/telescope-dap.nvim',
-      requires = {
-        'nvim-telescope/telescope.nvim',
-        'mfussenegger/nvim-dap'
-      },
-    }
+  use {
+    'nvim-treesitter/playground',
+    requires = { 'nvim-treesitter/nvim-treesitter' },
+  }
 
-    -- Syntax
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-        config = require 'plugins.configs.treesitter',
-    }
+  -- Git
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = require 'plugins.configs.gitsigns',
+  }
+  use { 'rbong/vim-flog' }
+  use { 'tpope/vim-fugitive' }
 
-    use {
-      'nvim-treesitter/playground',
-      requires = { 'nvim-treesitter/nvim-treesitter' },
-    }
+  -- LSP
+  use {
+    'williamboman/nvim-lsp-installer',
+    requires = {
+      'neovim/nvim-lspconfig',
+    },
+    config = require 'plugins.configs.lspinstaller',
+  }
 
-    -- Git
-    use {
-        'lewis6991/gitsigns.nvim',
-        requires = { 'nvim-lua/plenary.nvim' },
-        config = require 'plugins.configs.gitsigns',
-    }
-    use { 'rbong/vim-flog' }
-    use { 'tpope/vim-fugitive' }
+  use { 'j-hui/fidget.nvim', config = require 'plugins.configs.fidget' }
+  use { 'b0o/SchemaStore.nvim' }
 
-    -- LSP
-    use {
-        'williamboman/nvim-lsp-installer',
-        requires = {
-            'neovim/nvim-lspconfig',
-        },
-        config = require 'plugins.configs.lspinstaller',
-    }
+  -- Completion
 
-    use { 'j-hui/fidget.nvim', config = require 'plugins.configs.fidget'}
-    use { 'b0o/SchemaStore.nvim' }
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'onsails/lspkind-nvim',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp-document-symbol',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/vim-vsnip-integ',
+      'dmitmel/cmp-cmdline-history',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'rafamadriz/friendly-snippets',
+    },
+    config = require 'plugins.configs.cmp',
+  }
 
-    -- Completion
-
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = {
-            'hrsh7th/cmp-nvim-lsp',
-            'onsails/lspkind-nvim',
-            'hrsh7th/cmp-nvim-lua',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-nvim-lsp-document-symbol',
-            'hrsh7th/cmp-vsnip',
-            'hrsh7th/vim-vsnip',
-            'hrsh7th/vim-vsnip-integ',
-            'dmitmel/cmp-cmdline-history',
-            'hrsh7th/cmp-nvim-lsp-signature-help',
-            'rafamadriz/friendly-snippets',
-        },
-        config = require 'plugins.configs.cmp',
-    }
-
-    if PACKER_BOOTSTRAP then
-        require('packer').sync()
-    end
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
 end)
