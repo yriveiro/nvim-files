4d499934e6cad29ac85001029486430894633a02
Yago Riveiro <yago.riveiro@gmail.com>
2022-06-04 16:54:56 +0100

    chore(plugins) alpha cleanup

--- lua/plugins/configs/alpha.lua
+++ lua/plugins/configs/alpha.lua
@@ -1,36 +1,40 @@
 local ok, alpha = pcall(require, 'alpha')
 
 if not ok then
+  local u = require 'utils'
+  u.nok_plugin('alpha')
   return
 end
 
 local dsh = require 'alpha.themes.dashboard'
 
 local function button(sc, txt, keybind, keybind_opts)
-    local b = dsh.button(sc, txt, keybind, keybind_opts)
-    b.opts.hl = 'DashboardShortCut'
-    b.opts.hl_shortcut = 'DashboardHeader'
-    return b
+  local b = dsh.button(sc, txt, keybind, keybind_opts)
+  b.opts.hl = 'DashboardShortCut'
+  b.opts.hl_shortcut = 'DashboardHeader'
+  return b
 end
 
 dsh.section.header.val = {
-    [[ < Join Neovim, we have buffers! > ]],
-    [[ --------------------------------- ]],
-    [[        \    ,-^-.                 ]],
-    [[         \   !oYo!                 ]],
-    [[          \ /./=\.\______          ]],
-    [[               ##        )\/\      ]],
-    [[                ||-----w||         ]],
-    [[                ||      ||         ]],
-    [[                                   ]],
-    [[             Cowth Vader           ]],
+  [[ < Join Neovim, we have buffers! > ]],
+  [[ --------------------------------- ]],
+  [[        \    ,-^-.                 ]],
+  [[         \   !oYo!                 ]],
+  [[          \ /./=\.\______          ]],
+  [[               ##        )\/\      ]],
+  [[                ||-----w||         ]],
+  [[                ||      ||         ]],
+  [[                                   ]],
+  [[             Cowth Vader           ]],
 }
 
 dsh.section.buttons.val = {
-    button('\\ f f', '  Find File', '<cmd>Telescope find_files<cr>'),
-    button('\\ f o', '  Recents', '<cmd>Telescope oldfiles<cr>'),
-    button('\\ f r', '  Find Word', '<cmd>Telescope live_grep<cr>'),
-    button('\\ f m', '  Bookmarks', '<cmd>Telescope marks<cr>'),
+  button('\\ f f', '  Find File', '<cmd>Telescope find_files<CR>'),
+  button('\\ f o', '  Recents', '<cmd>Telescope oldfiles<CR>'),
+  button('\\ f r', '  Find Word', '<cmd>Telescope live_grep<CR>'),
+  button('\\ f m', '  Bookmarks', '<cmd>Telescope marks<CR>'),
+  button('\\ p s', '  Plugins Update', '<cmd>PackerSync<CR>'),
+  button('\\ p i', '  Plugins Install', '<cmd>PackerSync<CR>'),
 }
 
 dsh.section.footer.val = ''
