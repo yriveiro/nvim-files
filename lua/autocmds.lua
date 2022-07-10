c5f74d05274dd927a51a8dc8e827da97693cc826
Yago Riveiro <yago.riveiro@gmail.com>
2022-07-10 18:05:01 +0100

    fix(lsp) formatting_sync fn deprecation

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -55,7 +55,7 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("BufWrite", {
     pattern = "*.tf",
     callback = function()
-        vim.lsp.buf.formatting_sync()
+        vim.lsp.buf.format()
     end,
 })
 
