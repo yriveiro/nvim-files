8125cdc35fcd290d6937b80e124b8fa8784dfe28
Yago Riveiro <yago.riveiro@gmail.com>
2022-08-13 13:07:11 +0000

    feat(plugins): enable cmp-buffer all visible buffer

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -7,31 +7,31 @@ if not ok then
 end
 
 local icons = {
-    Class = 'ﴯ',
-    Color = '',
-    Constant = '',
-    Constructor = '⌘',
-    Enum = '',
-    EnumMember = '',
-    Event = '',
-    Field = 'ﰠ',
-    File = '',
-    Folder = '',
-    Function = '',
-    Interface = '',
-    Keyword = '',
-    Method = '',
-    Module = '',
-    Operator = '',
-    Property = 'ﰠ',
-    Reference = '',
-    Snippet = '',
-    Struct = 'פּ',
-    Text = '',
-    TypeParameter = '',
-    Unit = '',
-    Value = '',
-    Variable = '',
+  Class = 'ﴯ',
+  Color = '',
+  Constant = '',
+  Constructor = '⌘',
+  Enum = '',
+  EnumMember = '',
+  Event = '',
+  Field = 'ﰠ',
+  File = '',
+  Folder = '',
+  Function = '',
+  Interface = '',
+  Keyword = '',
+  Method = '',
+  Module = '',
+  Operator = '',
+  Property = 'ﰠ',
+  Reference = '',
+  Snippet = '',
+  Struct = 'פּ',
+  Text = '',
+  TypeParameter = '',
+  Unit = '',
+  Value = '',
+  Variable = '',
 }
 
 local menu = {
@@ -46,102 +46,113 @@ local menu = {
 }
 
 cmp.setup {
-    experimental = {
-        native_menu = false,
-        ghost_text = false,
-    },
-    confirmation = {
-        get_commit_characters = function()
-            return {}
-        end,
-    },
-    completion = {
-        completeopt = 'menu,menuone,noinsert',
-        keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
-        keyword_length = 1,
-    },
-    formatting = {
-        fields = { 'kind', 'abbr', 'menu' },
-        format = require'lspkind'.cmp_format({
-          mode = 'symbol_text', -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-          menu = menu,
-          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-          before = function (_, vim_item)
-            vim_item.menu = vim_item.kind
-            vim_item.kind = icons[vim_item.kind]
+  experimental = {
+    native_menu = false,
+    ghost_text = false,
+  },
+  confirmation = {
+    get_commit_characters = function()
+      return {}
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+    keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
+    keyword_length = 1,
+  },
+  formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
+    format = require 'lspkind'.cmp_format({
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      menu = menu,
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(_, vim_item)
+        vim_item.menu = vim_item.kind
+        vim_item.kind = icons[vim_item.kind]
 
-            return vim_item
+        return vim_item
+      end
+    })
+  },
+  snippet = {
+    expand = function(args)
+      vim.fn['vsnip#anonymous'](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-s>'] = cmp.mapping.complete({
+      config = {
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+        }
+      }
+    }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-q>'] = cmp.mapping.close(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn['vsnip#available'](1) == 1 then
+        vim.api.nvim_feedkeys(
+          vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true),
+          '',
+          true
+        )
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+        vim.api.nvim_feedkeys(
+          vim.api.nvim_replace_termcodes('<Plug>(vsnip-jump-prev)', true, true, true),
+          '',
+          true
+        )
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'vsnip' },
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
           end
-        })
+          return vim.tbl_keys(bufs)
+        end
+      }
     },
-    snippet = {
-        expand = function(args)
-            vim.fn['vsnip#anonymous'](args.body)
-        end,
-    },
-    mapping = {
-        ['<C-s>'] = cmp.mapping.complete({
-          config = {
-            sources = {
-              { name = 'nvim_lsp' },
-              { name = 'vsnip' },
-            }
-          }
-        }),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-q>'] = cmp.mapping.close(),
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif vim.fn['vsnip#available'](1) == 1 then
-                vim.api.nvim_feedkeys(
-                    vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true),
-                    '',
-                    true
-                )
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-                vim.api.nvim_feedkeys(
-                    vim.api.nvim_replace_termcodes('<Plug>(vsnip-jump-prev)', true, true, true),
-                    '',
-                    true
-                )
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true })
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'vsnip' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'nvim_lua' },
-    },
-    preselect = cmp.PreselectMode.None,
+    { name = 'path' },
+    { name = 'nvim_lua' },
+  },
+  preselect = cmp.PreselectMode.None,
 }
 
 cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' },
-    },
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
 })
 
 cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'cmdline' },
-        { name = 'path' },
-        { name = 'cmdline_history' },
-    }),
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'cmdline' },
+    { name = 'path' },
+    { name = 'cmdline_history' },
+  }),
 })
