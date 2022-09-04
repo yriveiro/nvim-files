0e38f1ebb72cfd6fcb6555e7e3386a8212ba769c
Yago Riveiro <yago.riveiro@gmail.com>
2022-09-04 17:39:02 +0100

    feat: improve loading of impatient

--- init.lua
+++ init.lua
@@ -1,14 +1,26 @@
 -- Yago Riveiro's vim configuration
 --
---  Overarching Philosophy:
+-- Overarching Philosophy:
 -- 1. Prefer the defaults
 -- 2. Extend functionality where needed
 -- 3. Change functionality only when it doesn't make sense
 
-require 'impatient'
-require 'settings'
-require 'plugins'
-require 'lsp'
-require 'mappings'
-require 'autocmds'
-require 'commands'
+local ok, impatient = pcall(require, 'impatient')
+
+if ok then
+  impatient.enable_profile()
+end
+
+for _, source in ipairs {
+  'settings',
+  'plugins',
+  'lsp',
+  'mappings',
+  'autocmds',
+  'commands',
+} do
+  local ok, err = pcall(require, source)
+  if not ok then
+    vim.api.nvim_err_writeln('Failed to load ' .. source .. '\n\n' .. err)
+  end
+end
