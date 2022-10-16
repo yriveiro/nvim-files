0ec10e1779d0e36816408d4eb47687b8eff838ea
Yago Riveiro <yago.riveiro@gmail.com>
2022-10-16 15:52:55 +0100

    feat: add section filename to lualine

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,12 +1,17 @@
 require('lualine').setup {
-	sections = {
-		lualine_c = {
-			'lsp_progress'
-		}
-	},
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      },
+      'lsp_progress',
+    }
+  },
   extensions = {
     'neo-tree',
     'nvim-dap-ui',
     'quickfix',
-  }
+  },
+  globalstatus = true,
 }
