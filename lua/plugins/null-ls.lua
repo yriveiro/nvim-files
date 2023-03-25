be8e9424f45e204801b53aa3f561d4c15bece7a8
Yago Riveiro <yago.riveiro@farfetch.com>
2023-03-25 11:07:37 +0000

    feat: add new diagnostics to null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -10,12 +10,13 @@ return {
     null_ls.setup {
       sources = {
         null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.checkmake,
+        null_ls.builtins.diagnostics.hadolint,
+        null_ls.builtins.formatting.beautysh,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.yapf,
-        null_ls.builtins.formatting.beautysh,
-        null_ls.builtins.formatting.hadolint,
       },
     }
   end,
