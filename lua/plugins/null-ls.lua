c14687f3a014faf94fc9e27200c746d167135670
Yago Riveiro <yago.riveiro@farfetch.com>
2023-06-12 09:23:11 +0100

    chore: add null-ls sources

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -12,10 +12,12 @@ return {
         null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.checkmake,
         null_ls.builtins.diagnostics.hadolint,
+        null_ls.builtins.diagnostics.npm_groovy_lint,
         null_ls.builtins.formatting.beautysh,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.trim_whitespace,
         null_ls.builtins.formatting.yamlfmt.with({
           extra_args = {
             "-formatter",
