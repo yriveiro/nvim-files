ee191f29cf3262b0326cb52ce89ff6d1458fe173
Yago Riveiro <yago.riveiro@gmail.com>
2023-08-09 10:14:33 +0100

    chore: add custom mason configurations

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -207,4 +207,13 @@ return {
       },
     },
   },
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        'tflint',
+      })
+    end,
+  },
 }
