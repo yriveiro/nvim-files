b8baf635be446613ac4f908f07547cc74586c61a
Yago Riveiro <yago.riveiro@farfetch.com>
2023-08-28 09:48:09 +0100

    feat: configure vim folding behaviour

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.laststatus = 3 -- Display the status line always. Improves window picker behaviour
+vim.o.foldcolumn = '1'
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = -1
+vim.o.laststatus = 3 -- Display the status line always. Improves window picker behaviour
 vim.o.swapfile = false
