1cf2b65455e55edddf4e044988891952f2ff2a98
Yago Riveiro <yago.riveiro@farfetch.com>
2023-08-31 14:20:34 +0100

    feat: teawk UFO for neotree and symbols

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,11 @@
 -- Add any additional autocmds here
 
 vim.cmd [[highlight ColorColumn guibg=#FF8C00]]
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'neo-tree', 'Outline' },
+  callback = function()
+    require('ufo').detach()
+    vim.opt_local.foldenable = false
+  end,
+})
