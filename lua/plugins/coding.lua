4059721208551d735c727f99a8ea0a088f528742
Yago Riveiro <yago.riveiro@gmail.com>
2023-11-26 23:24:56 +0000

    fix: headlines highlights

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -10,4 +10,13 @@ return {
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {}))
     end,
   },
+  {
+    'lukas-reineke/headlines.nvim',
+    opts = {
+      markdown = {
+        fat_headline_upper_string = '▃',
+        fat_headline_lower_string = '━',
+      },
+    },
+  },
 }
