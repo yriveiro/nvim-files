965b91658f00ec1ce3126e0dd8e90bc813b41514
Yago Riveiro <yago.riveiro@gmail.com>
2023-12-19 15:15:38 +0000

    feat: adding neorg plugin

---
+++ lua/plugins/notes.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'nvim-neorg/neorg',
+    build = ':Neorg sync-parsers',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {}, -- Loads default behaviour
+          ['core.concealer'] = {}, -- Adds pretty icons to your documents
+          ['core.dirman'] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                work = '~/notes/work',
+                home = '~/notes/home',
+                oss = '~/notes/oss',
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+}
