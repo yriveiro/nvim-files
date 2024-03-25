ecdcb611bb2b3bdf12bd9ca86260aa8c38e094e3
Yago Riveiro <yago.riveiro@farfetch.com>
2024-03-25 09:33:34 +0000

    feature: add obsidian plugin
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

---
+++ lua/plugins/productivity.lua
@@ -0,0 +1,129 @@
+return {
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    event = {
+      'BufReadPre ' .. vim.fn.expand '~' .. '/vaults/*/**.md',
+      'BufNewFile ' .. vim.fn.expand '~' .. '/vaults/*/**.md',
+    },
+    keys = {
+      {
+        mode = { 'n' },
+        '\\ok',
+        '<cmd>ObsidianDailies<cr>',
+        desc = 'Open dailies',
+      },
+      {
+        mode = { 'n' },
+        '\\oc',
+        '<cmd>ObsidianNew<cr>',
+        desc = 'Create new note',
+      },
+      {
+        mode = { 'n' },
+        '\\oq',
+        '<cmd>ObsidianQuickSwitch<cr>',
+        desc = 'Switch Notes',
+      },
+      {
+        mode = { 'n' },
+        '\\os',
+        '<cmd>ObsidianSearch<cr>',
+        desc = 'Search Notes',
+      },
+    },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'work',
+          path = '~/vaults/work',
+        },
+      },
+      notes_subdir = 'notes',
+      new_notes_location = 'notes_subdir',
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = 'notes/dailies',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = '%Y-%m-%d',
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = '%B %-d, %Y',
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ['gf'] = {
+          action = function()
+            return require('obsidian').util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ['\\ol'] = {
+          action = function()
+            return require('obsidian').util.toggle_checkbox()
+          end,
+          opts = { buffer = true, desc = 'Toogle Checkboxes' },
+        },
+      },
+      templates = {
+        subdir = 'templates',
+        date_format = '%Y-%m-%d',
+        time_format = '%H:%M',
+        -- A map for custom variables, the key should be the variable and the value a function
+        substitutions = {},
+      },
+      ui = {
+        enable = true, -- set to false to disable all additional syntax features
+        update_debounce = 200, -- update delay after a text change (in milliseconds)
+        -- Define how various check-boxes are displayed
+        checkboxes = {
+          -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+          [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+          ['x'] = { char = '', hl_group = 'ObsidianDone' },
+          ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+          ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+          -- Replace the above with this if you don't have a patched font:
+          -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+          -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+          -- You can also add more custom ones...
+        },
+        -- Use bullet marks for non-checkbox lists.
+        bullets = { char = '•', hl_group = 'ObsidianBullet' },
+        external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+        -- Replace the above with this if you don't have a patched font:
+        -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = 'ObsidianRefText' },
+        highlight_text = { hl_group = 'ObsidianHighlightText' },
+        tags = { hl_group = 'ObsidianTag' },
+        block_ids = { hl_group = 'ObsidianBlockID' },
+        hl_groups = {
+          -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+          ObsidianTodo = { bold = true, fg = '#f78c6c' },
+          ObsidianDone = { bold = true, fg = '#89ddff' },
+          ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+          ObsidianTilde = { bold = true, fg = '#ff5370' },
+          ObsidianBullet = { bold = true, fg = '#89ddff' },
+          ObsidianRefText = { underline = true, fg = '#c792ea' },
+          ObsidianExtLinkIcon = { fg = '#c792ea' },
+          ObsidianTag = { italic = true, fg = '#89ddff' },
+          ObsidianBlockID = { italic = true, fg = '#89ddff' },
+          ObsidianHighlightText = { bg = '#75662e' },
+        },
+      },
+    },
+  },
+}
