61822f006544881892f7c720350cde29289af507
Yago Riveiro <yago.riveiro@farfetch.com>
2024-03-28 16:35:51 +0000

    feat: configure image folder for obsidian
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -124,6 +124,22 @@ return {
           ObsidianHighlightText = { bg = '#75662e' },
         },
       },
+      attachments = {
+        -- The default folder to place images in via `:ObsidianPasteImg`.
+        -- If this is a relative path it will be interpreted as relative to the vault root.
+        -- You can always override this per image by passing a full path to the command instead of just a filename.
+        img_folder = 'assets/imgs', -- This is the default
+        -- A function that determines the text to insert in the note when pasting an image.
+        -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+        -- This is the default implementation.
+        ---@param client obsidian.Client
+        ---@param path obsidian.Path the absolute path to the image file
+        ---@return string
+        img_text_func = function(client, path)
+          path = client:vault_relative_path(path) or path
+          return string.format('![%s](%s)', path.name, path)
+        end,
+      },
     },
   },
 }
