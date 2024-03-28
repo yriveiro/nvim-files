5fc56f45167cf9bb18433739e42b72f1b0c89f2a
Yago Riveiro <yago.riveiro@farfetch.com>
2024-03-28 16:37:18 +0000

    feat: add emoji cmp
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,13 +5,29 @@ return {
     dependencies = {
       'chrisgrieser/cmp_yanky',
       'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-emoji',
     },
     opts = function(_, opts)
       local cmp = require 'cmp'
       local lspkind = require 'lspkind'
 
-      opts.sources =
-        cmp.config.sources(vim.list_extend(opts.sources, { { name = 'cmp_yanky' }, { name = 'nvim_lua' } }))
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = 'cmp_yanky' },
+        { name = 'nvim_lua' },
+        { name = 'emoji' },
+        {
+          name = 'buffer',
+          option = {
+            get_bufnrs = function()
+              local bufs = {}
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+              end
+              return vim.tbl_keys(bufs)
+            end,
+          },
+        },
+      }))
 
       opts.formatting.format = lspkind.cmp_format {
         mode = 'symbol_text',
@@ -20,11 +36,12 @@ return {
         menu = {
           buffer = '[Buffer]',
           cmp_yanky = '[Yanky]',
+          emoji = '[Emoji]',
           luasnip = '[LuaSnip]',
           nvim_lsp = '[LSP]',
           nvim_lua = '[Lua]',
-          snippets = '[snippets]',
           path = '[Path]',
+          snippets = '[snippets]',
         },
       }
     end,
