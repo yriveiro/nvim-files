9abc43d7a34edd7d9ffd9822c7e7e378e686d008
Yago Riveiro <yago.riveiro@farfetch.com>
2024-08-07 11:29:05 +0100

    chore(cleanup): disable plugins
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 return {
   { 'akinsho/bufferline.nvim', enabled = false },
   { 'folke/flash.nvim', enabled = false },
-  { 'lukas-reineke/headlines.nvim', enabled = false },
-  -- { 'kevinhwang91/nvim-ufo', enabled = false },
 }
