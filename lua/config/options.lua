7e80bf8fa60e5f16f1bfb4093e910f445ab7fddd
Yago Riveiro <yago.riveiro@gmail.com>
2024-09-06 20:54:52 +0100

    feat: configure diagram
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,15 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.foldcolumn = '1'
-vim.o.foldenable = true
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = -1
-vim.o.laststatus = 3 -- Display the status line always. Improves window picker behaviour
-vim.o.swapfile = false
 vim.opt.foldcolumn = '1'
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = -1
+vim.opt.laststatus = 3
 vim.opt.swapfile = false
 vim.opt.cmdheight = 0
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -308,5 +308,23 @@ return {
     dependencies = {
       '3rd/image.nvim',
     },
+    config = function()
+      require('diagram').setup {
+        integrations = {
+          require 'diagram.integrations.markdown',
+        },
+        renderer_options = {
+          mermaid = {
+            theme = 'forest',
+          },
+          plantuml = {
+            charset = 'utf-8',
+          },
+          d2 = {
+            theme_id = 1,
+          },
+        },
+      }
+    end,
   },
 }
