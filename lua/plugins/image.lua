a207a89bc06dc8718cc1e2ce6a86514e88d2bc48
Yago Riveiro <yago.riveiro@farfetch.com>
2024-09-09 09:20:18 +0100

    feat: move image related plugins to its on namespace
    Signed-off-by: Yago Riveiro <yago.riveiro@farfetch.com>

---
+++ lua/plugins/image.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    '3rd/image.nvim',
+    opts = {
+      backend = 'kitty',
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+          filetypes = { 'markdown', 'vimwiki' },
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      kitty_method = 'unicode-placeholders',
+    },
+  },
+  {
+    '3rd/diagram.nvim',
+    dependencies = {
+      '3rd/image.nvim',
+    },
+    config = function()
+      require('diagram').setup {
+        integrations = {
+          require 'diagram.integrations.markdown',
+        },
+        renderer_options = {
+          mermaid = {
+            theme = 'forest',
+          },
+          plantuml = {
+            charset = 'utf-8',
+          },
+          d2 = {
+            theme_id = 1,
+          },
+        },
+      }
+    end,
+  },
+  {
+    'HakonHarnes/img-clip.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      { '<LocalLeader>p', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+    },
+  },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -282,49 +282,4 @@ return {
     'kevinhwang91/nvim-bqf',
     lazy = 'VeryLazy',
   },
-  {
-    '3rd/image.nvim',
-    event = 'VeryLazy',
-    opts = {
-      backend = 'kitty',
-      integrations = {
-        markdown = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          download_remote_images = true,
-          only_render_image_at_cursor = false,
-          filetypes = { 'markdown', 'vimwiki' },
-        },
-      },
-      max_width = nil,
-      max_height = nil,
-      max_width_window_percentage = nil,
-      max_height_window_percentage = 50,
-      kitty_method = 'normal',
-    },
-  },
-  {
-    '3rd/diagram.nvim',
-    dependencies = {
-      '3rd/image.nvim',
-    },
-    config = function()
-      require('diagram').setup {
-        integrations = {
-          require 'diagram.integrations.markdown',
-        },
-        renderer_options = {
-          mermaid = {
-            theme = 'forest',
-          },
-          plantuml = {
-            charset = 'utf-8',
-          },
-          d2 = {
-            theme_id = 1,
-          },
-        },
-      }
-    end,
-  },
 }
