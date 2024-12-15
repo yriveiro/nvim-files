fa7f4108270e224df4f42b592dbd27fe420ba987
Yago Riveiro <yago.riveiro@gmail.com>
2024-12-15 15:09:23 +0000

    feat: use blink instead of nvim-cmp
    Signed-off-by: Yago Riveiro <yago.riveiro@gmail.com>

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,56 +1,40 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'chrisgrieser/cmp_yanky',
-      'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/cmp-emoji',
-    },
-    opts = function(_, opts)
-      local cmp = require 'cmp'
-      local lspkind = require 'lspkind'
-
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = 'cmp_yanky', priority = -100 },
-        { name = 'nvim_lua' },
-        { name = 'emoji' },
-        {
-          name = 'buffer',
-          option = {
-            get_bufnrs = function()
-              local bufs = {}
-              for _, win in ipairs(vim.api.nvim_list_wins()) do
-                bufs[vim.api.nvim_win_get_buf(win)] = true
-              end
-              return vim.tbl_keys(bufs)
-            end,
+    'saghen/blink.cmp',
+    opts = {
+      appearance = {
+        use_nvim_cmp_as_default = true,
+      },
+      completion = {
+        menu = {
+          draw = {
+            columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind' } },
           },
         },
-      }))
-
-      opts.formatting.format = lspkind.cmp_format {
-        mode = 'symbol_text',
-        max_width = 50,
-        ellipsis_char = '...',
-        menu = {
-          buffer = '[Buffer]',
-          cmp_yanky = '[Yanky]',
-          emoji = '[Emoji]',
-          luasnip = '[LuaSnip]',
-          nvim_lsp = '[LSP]',
-          nvim_lua = '[Lua]',
-          path = '[Path]',
-          snippets = '[snippets]',
-        },
-      }
-    end,
+      },
+    },
   },
   {
     'folke/lazydev.nvim',
+    ft = 'lua',
+    dependencies = {
+      { 'gonstoll/wezterm-types', lazy = true },
+    },
     opts = {
       library = {
         { path = 'wezterm-types', mods = { 'wezterm' } },
+      },
+    },
+  },
+  {
+    'folke/trouble.nvim',
+    cmd = { 'Trouble' },
+    opts = {
+      pinned = true,
+      win = {
+        size = {
+          width = 100,
+        },
       },
     },
   },
